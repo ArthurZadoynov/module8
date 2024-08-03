26fce096136ae81308864df6f3dd7b4e7882206c
Arthur Zadoynov <artyrrrr95@mail.ru>
2024-08-03 17:57:02 +0300

    first edit

---
+++ dist/index.672d4772.js
@@ -0,0 +1,3 @@
+console.log("\u041F\u0440\u0438\u0432\u0435\u0442, \u041C\u0438\u0440!");
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ server.js
@@ -0,0 +1,4 @@
+const math = require ('./math');
+
+console.log(math.add(7,3));
+console.log(math.subtract(8,4));
